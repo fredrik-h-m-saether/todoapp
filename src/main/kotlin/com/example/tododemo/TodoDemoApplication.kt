af3b3f88a544c6771e8c4f4c737f57f46c37b34c
Øystein Opedal <opedaoys@ysteins-MacBook-Pro.local>
2024-06-05 16:02:05 +0200

    Initial commit

--- src/main/kotlin/com/example/tododemo/TodoDemoApplication.kt
+++ src/main/kotlin/com/example/tododemo/TodoDemoApplication.kt
@@ -2,15 +2,6 @@ package com.example.tododemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RequestParam
-import java.util.UUID
-
 
 @SpringBootApplication
 class TodoDemoApplication
--- src/test/kotlin/com/example/tododemo/TodoDemoApplicationTests.kt
+++ src/test/kotlin/com/example/tododemo/TodoDemoApplicationTests.kt
@@ -10,5 +10,4 @@ class TodoDemoApplicationTests {
     fun contextLoads() {
 
     }
-
 }
